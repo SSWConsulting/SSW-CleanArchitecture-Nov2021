6044fcd8dd31c5f992a2afaff4f2e682b9404248
Jason Taylor <jasontaylor@ssw.com.au>
2021-11-22 14:23:48 +1100

    BeyondBasics-01

---
+++ CaWorkshop/CaWorkshop.WebUI/ConfigureServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaWorkshop.WebUI
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddWebUIServices(this IServiceCollection services)
+        {
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddOpenApiDocument(configure =>
+            {
+                configure.Title = "CaWorkshop API";
+            });
+
+            return services;
+        }
+    }
+}
